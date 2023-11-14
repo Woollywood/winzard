4b0d1367ee82b30e9a75d146c15a7d9e920e0192
woollywood <Gerbert.Wells969@yandex.ru>
2023-11-14 16:02:01 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -2472,6 +2472,34 @@
                     }
                 }
             });
+            const gameSlider = document.querySelectorAll("[data-game-slider]");
+            gameSlider.forEach((slider => {
+                new Swiper(slider, {
+                    observer: true,
+                    observeParents: true,
+                    spaceBetween: 16,
+                    speed: 800,
+                    navigation: {
+                        prevEl: slider.closest("[data-game-slider-root]").querySelector(".slider-arrows__arrow-prev"),
+                        nextEl: slider.closest("[data-game-slider-root]").querySelector(".slider-arrows__arrow-next")
+                    },
+                    breakpoints: {
+                        320: {
+                            slidesPerView: 3,
+                            spaceBetween: 10
+                        },
+                        768: {
+                            slidesPerView: 4
+                        },
+                        992: {
+                            slidesPerView: 5
+                        },
+                        1268: {
+                            slidesPerView: 6
+                        }
+                    }
+                });
+            }));
         }
         window.addEventListener("load", (function(e) {
             initSliders();
