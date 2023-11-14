7c6f12e62f8c356a7281e9bd9a7ab875c9b6be1b
woollywood <Gerbert.Wells969@yandex.ru>
2023-11-14 06:08:41 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -268,6 +268,61 @@
             panelClass: "accordion__panel",
             activeClass: "active"
         });
+        function initSliders() {
+            if (document.querySelector(".games-slider")) new Swiper(".games-slider", {
+                observer: true,
+                observeParents: true,
+                speed: 800,
+                navigation: {
+                    prevEl: ".game-launcher__slider-wrapper--games-slider .slider-arrows__arrow-prev",
+                    nextEl: ".game-launcher__slider-wrapper--games-slider .slider-arrows__arrow-next"
+                },
+                breakpoints: {
+                    320: {
+                        slidesPerView: 3,
+                        spaceBetween: 10
+                    },
+                    768: {
+                        slidesPerView: 4
+                    },
+                    992: {
+                        slidesPerView: 5,
+                        spaceBetween: 16
+                    },
+                    1268: {
+                        slidesPerView: 6
+                    }
+                }
+            });
+            if (document.querySelector(".providers-slider")) new Swiper(".providers-slider", {
+                observer: true,
+                observeParents: true,
+                speed: 800,
+                navigation: {
+                    prevEl: ".game-launcher__slider-wrapper--providers-slider .slider-arrows__arrow-prev",
+                    nextEl: ".game-launcher__slider-wrapper--providers-slider .slider-arrows__arrow-next"
+                },
+                breakpoints: {
+                    320: {
+                        slidesPerView: 3,
+                        spaceBetween: 10
+                    },
+                    768: {
+                        slidesPerView: 4
+                    },
+                    992: {
+                        slidesPerView: 5,
+                        spaceBetween: 16
+                    },
+                    1268: {
+                        slidesPerView: 6
+                    }
+                }
+            });
+        }
+        window.addEventListener("load", (function(e) {
+            initSliders();
+        }));
         let addWindowScrollEvent = false;
         setTimeout((() => {
             if (addWindowScrollEvent) {
