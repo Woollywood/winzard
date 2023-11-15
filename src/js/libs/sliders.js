b0bbbc85a2861c02358eef2394cf21b9df92aa3e
woollywood <Gerbert.Wells969@yandex.ru>
2023-11-15 06:29:20 +0300

    old slider fix

--- src/js/libs/sliders.js
+++ src/js/libs/sliders.js
@@ -59,6 +59,77 @@ function initSliders() {
 		});
 	}
 
+	if (document.querySelector('.main-block__slider')) {
+		new Swiper('.main-block__slider', {
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			speed: 800,
+			loop: true,
+			autoplay: {
+				delay: 3000,
+			},
+
+			scrollbar: {
+				el: '.main-block__slider-scrollbar',
+			},
+		});
+	}
+
+	if (document.querySelector('.footbal_cards')) {
+		const allfootbalmatcheslist = new Swiper('.footbal_cards', {
+			slidesPerView: 6,
+			spaceBetween: 15,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			breakpoints: {
+				200: {
+					slidesPerView: 2,
+					spaceBetween: 50,
+				},
+				1024: {
+					slidesPerView: 6,
+					spaceBetween: 50,
+				},
+			},
+		});
+	}
+
+	if (document.querySelector('.bannersSwiper')) {
+		const swiper = new Swiper('.bannersSwiper', {
+			effect: 'coverflow',
+			grabCursor: true,
+			centeredSlides: true,
+			slidesPerView: 'auto',
+			coverflowEffect: {
+				rotate: 50,
+				stretch: 0,
+				depth: 100,
+				modifier: 1,
+				slideShadows: true,
+			},
+			pagination: {
+				el: '.swiper-pagination',
+			},
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+				},
+				768: {
+					slidesPerView: 2,
+				},
+				1024: {
+					effect: 'default',
+					centeredSlides: false,
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+			},
+		});
+	}
+
 	const gameSlider = document.querySelectorAll('[data-game-slider]');
 	gameSlider.forEach((slider) => {
 		new Swiper(slider, {
